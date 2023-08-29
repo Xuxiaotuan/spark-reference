8169dd602f7013c5c9fe9975f6ec9a39e8c91065
Xuxiaotuan <jia_3020502@qq.com>
2023-08-29 11:24:29 +0800

    add loacl spark config

--- src/main/scala/cn/xuyinyin/magic/SimpleApp.scala
+++ src/main/scala/cn/xuyinyin/magic/SimpleApp.scala
@@ -6,7 +6,7 @@ object SimpleApp {
 
   def main(args: Array[String]): Unit = {
     val logFile = "/Users/xjw/software/spark/spark-3.4.1-bin-hadoop3-scala2.13/README.md" // Should be some file on your system
-    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val spark = SparkSession.builder.appName("Simple Application").master("local").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
